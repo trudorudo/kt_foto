fead02aebe423cdf8f1b061ac54f87b022f7acf4
Kristina Trudorudo <ktrudorudo@kristinas-mbp.mynetworksettings.com>
2024-05-14 09:04:22 -0400

    mobile

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -5,8 +5,8 @@ import insta from "../assets/me/insta.png";
 const About = () => {
     const INSTA_URL = "https://www.instagram.com/_ktfoto/";
     return (
-        <div className="flex bg-black mt-14 pt-28">
-            <div className="pl-28">
+        <div className="flex flex-col lg:flex-row bg-black mt-14 pt-28">
+            <div className="pl-16 md:pl-28">
                 <img src={me} alt="me" className="h-[15rem]  rounded-full object-cover" />
             </div>
             <div className=" h-screen text-white pl-8 pt-4 space-grotesk-style">
@@ -15,12 +15,12 @@ const About = () => {
                 I love faces, streets and to play with light.<br />
                 And it's a little attempt to show the beauty I see...<br />
                 Shoot me an email if you want to collaborate or get my prints.
-                <div className="pt-10 space-grotesk-style flex">
+                <div className="pt-10 space-grotesk-style flex flex-col lg:flex-row">
                     <div>
                         <div>DIRECT CONTACT</div>
                         <div>k.trudorudo@gmail.com</div>
                     </div>
-                    <div className="ml-auto mt-2">
+                    <div className="md:ml-auto mt-2">
                         <a href={INSTA_URL} target="_blank" rel="noopener noreferrer">
                             <img src={insta} alt="insta" className="h-10" />
                         </a>
